55439f89522c5eb0bd7da14903a6f334cd872b24
Preston Davis <prestonseandavis101@yahoo.com>
2020-08-31 23:25:27 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,14 @@
 class Dog
-  def name = (dog_name)
-    
+  def name = (dogs_name)
+    @this_dogs_name = dogs_name
 end
 
+def name
+  @this_dogs_name
+end 
+end
 
+fido = Dog.new 
+fido.name = "Fido"
+
+puts fido.name
